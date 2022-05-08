796db0f474a84ea4047b624950f9647430668a2a
ashupandey358 <you@example.com>
2022-05-09 01:36:24 +0530

    1st phase complete

---
+++ Travel-Log-App-Rest/travelLogApp/src/test/java/com/ashucompany/travelLogApp/TravelLogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashucompany.travelLogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelLogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
